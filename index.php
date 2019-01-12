96b9f82d97864560fc21544fd9047c94e9814dca
Robert Kuhlig <16413104+Kuhlig@users.noreply.github.com>
2019-01-12 17:23:36 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
-echo "test";
+	define('__ROOT__', dirname(__FILE__)); 
 
+	require_once(__DIR__.'/inc/api.secret.inc.php');
+	require_once(__ROOT__.'/inc/api.inc.php');
+	require_once(__ROOT__.'/inc/load_ato_data.inc.php');	
 ?>
