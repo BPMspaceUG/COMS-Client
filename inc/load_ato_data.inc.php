65593000c26f644ec8776c05c1ea2da67d6ee83c
Boban Ilievski <bilievski@ssidecisions.com>
2019-02-19 13:11:56 +0100

    Path update and gitignore update

--- inc/load_ato_data.inc.php
+++ inc/load_ato_data.inc.php
@@ -5,6 +5,7 @@
 <script src="//stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 <link rel="stylesheet" href="//use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
 <link rel="stylesheet" type="text/css" href="/css/main.css"/>
+<link rel="stylesheet" type="text/css" href="//ico-cert.org/assets/css/style.css"/>
 
 <?php
 session_start();
@@ -275,7 +276,7 @@ if (ctype_xdigit($PARTID_MD5) && strlen($PARTID_MD5) == 32 && strlen($PARTID) ==
     // MatrNr Postfix calculate
     $PARTID_MD5_first5 = substr($PARTID_MD5, 0, 5);
     $matNr_postfix = substr(base_convert($PARTID_MD5_first5, 16, 10), 0, 3);
-    //echo "PostFIX MatrkNr = ".$matNr_postfix."<br/>";
+    //echo "PostFI MatrkNr = ".$matNr_postfix."<br/>";
     $matNr = $PARTID.substr(base_convert($PARTID_MD5_first5, 16, 10), 0, 3);
     $matNr = str_pad(strtoupper(base_convert($matNr, 10, 32)), 8, "0", STR_PAD_LEFT);
     //echo $actionurl;
